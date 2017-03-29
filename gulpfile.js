004deea4ad62730fa314f7642d6305a3cbf61654
John Hegman <jhegman@uvm.edu>
2017-03-28 20:56:41 -0800

    gitignore

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+const elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ |
+ */
+
+elixir((mix) => {
+	mix.sass('./src/sass/app.scss')
+		.webpack('./src/js/app.js')
+});
